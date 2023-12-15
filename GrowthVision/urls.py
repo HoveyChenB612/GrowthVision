039d10db4234e9e1e3185c0c78f8c9f993804003
HoveyChenB612 <chenhao852@icloud.com>
2023-12-15 10:55:57 +0800

    修改文章检测的请求方式

--- GrowthVision/urls.py
+++ GrowthVision/urls.py
@@ -19,49 +19,41 @@ from django.urls import path
 from mainsite.views import index, account, user, hot, task, check
 
 urlpatterns = [
-	# path('admin/', admin.site.urls),
-
-	# 主页
-	path("index/", index.index),
-	path("index/total/bilibili/", index.index_total_bilibili),
-	path("index/total/zhihu/", index.index_total_zhihu),
-	path("index/total/baijiahao/", index.index_total_baijiahao),
-	path("index/echarts/", index.index_echarts),
-
-	# 平台账号
-	path("account/auth/list/", account.account_auth_list),
-	path("account/auth/detail/", account.account_auth_detail),
-	path("account/auth/douyin/", account.account_auth_douyin),
-	path("account/auth/bilibili/", account.account_auth_bilibili),
-	path("account/auth/zhihu/", account.account_auth_zhihu),
-	path("account/auth/baijiahao/", account.account_auth_baijiahao),
-	path("account/auth/get/", account.account_auth_get),
-	path("account/delete/", account.account_delete),
-	path("account/data/list/", account.account_data_list),
-	path("account/data/get/", account.account_data_get),
-	path("account/data/update/", account.account_data_update),
-	path("account/refresh/", account.account_auth_refresh),
-	path("account/auth/detail/echarts/", account.account_auth_detail_echarts),
-
-	# 用户账号
-	path("login/", user.login),
-	path("image/code/", user.image_code),
-	path("logout/", user.user_logout),
-	path("register/", user.register),
-	path("edit_pwd/", user.edit_pwd),
-
-	# 热词搜索
-	path("hot/list/", hot.hot_list),
-
-	# 主页
-	path("", index.main),
-
-	# 敏感词检测
-	path("check/article/", check.check_article),
-	path("check/article/get/words/", check.check_article_get_words),
-	path("check/article/save/words/", check.check_article_save_words),
-	path("check/article/checking/", check.check_article_checking)
-
-	# 测试数据定时任务
-	# path("history/data/update/", task.history_data_update)
+    # path('admin/', admin.site.urls),
+    # 主页
+    path("index/", index.index),
+    path("index/total/bilibili/", index.index_total_bilibili),
+    path("index/total/zhihu/", index.index_total_zhihu),
+    path("index/total/baijiahao/", index.index_total_baijiahao),
+    path("index/echarts/", index.index_echarts),
+    # 平台账号
+    path("account/auth/list/", account.account_auth_list),
+    path("account/auth/detail/", account.account_auth_detail),
+    path("account/auth/douyin/", account.account_auth_douyin),
+    path("account/auth/bilibili/", account.account_auth_bilibili),
+    path("account/auth/zhihu/", account.account_auth_zhihu),
+    path("account/auth/baijiahao/", account.account_auth_baijiahao),
+    path("account/auth/get/", account.account_auth_get),
+    path("account/delete/", account.account_delete),
+    path("account/data/list/", account.account_data_list),
+    path("account/data/get/", account.account_data_get),
+    path("account/data/update/", account.account_data_update),
+    path("account/refresh/", account.account_auth_refresh),
+    path("account/auth/detail/echarts/", account.account_auth_detail_echarts),
+    # 用户账号
+    path("login/", user.login),
+    path("image/code/", user.image_code),
+    path("logout/", user.user_logout),
+    path("register/", user.register),
+    path("edit_pwd/", user.edit_pwd),
+    # 热词搜索
+    path("hot/list/", hot.hot_list),
+    # 主页
+    path("", index.main),
+    # 敏感词检测
+    path("check/article/", check.check_article),
+    path("check/article/get/words/", check.check_article_get_words),
+    path("check/article/save/words/", check.check_article_save_words),
+    # 测试数据定时任务
+    # path("history/data/update/", task.history_data_update)
 ]
--- mainsite/views/check.py
+++ mainsite/views/check.py
@@ -7,76 +7,52 @@ from datetime import datetime
 
 
 def check_article(request):
-	"""文章检测"""
-	# 前端选中标签与头部标签
-	active_check = ""
-	header_label = ""
-	if request.path == "/check/article/":
-		active_check = "active"
-		header_label = "文章检测"
+    """文章检测"""
+    # 前端选中标签与头部标签
+    active_check = ""
+    header_label = ""
+    if request.path == "/check/article/":
+        active_check = "active"
+        header_label = "文章检测"
 
-	context = {
-		"active_check": active_check,
-		"header_label": header_label,
-	}
-	return render(request, "check_article.html", context)
+    context = {
+        "active_check": active_check,
+        "header_label": header_label,
+    }
+    return render(request, "check_article.html", context)
 
 
 def check_article_get_words(request):
-	"""获取敏感词接口"""
+    """获取敏感词接口"""
 
-	# 获取当前登陆的用户uid
-	uid = request.session.get("info").get("uid")
-	words = ""
-	exists = models.SensitiveWords.objects.filter(uid=uid).exists()
-	if exists:
-		queryset = models.SensitiveWords.objects.filter(uid=uid).first()
-		words = queryset.words
+    # 获取当前登陆的用户uid
+    uid = request.session.get("info").get("uid")
+    words = ""
+    exists = models.SensitiveWords.objects.filter(uid=uid).exists()
+    if exists:
+        queryset = models.SensitiveWords.objects.filter(uid=uid).first()
+        words = queryset.words
 
-	return JsonResponse({"status": True, "words": words})
+    return JsonResponse({"status": True, "words": words})
 
 
 @csrf_exempt
 def check_article_save_words(request):
-	"""保存敏感词接口"""
-	# 获取当前登陆的用户uid
-	uid = request.session.get("info").get("uid")
-	tag_list_js = request.POST.get("list")
-	tag_list = json.loads(tag_list_js)
-	words = ""
-	for word in tag_list:
-		words += f"{word['tag']};"
-	update_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-	group = 1  # 埋点分组
-	data_dict = {
-		"group": group,
-		"update_time": update_time,
-		"words": words
-	}
-	exists = models.SensitiveWords.objects.filter(uid=uid).exists()
-	if exists:
-		models.SensitiveWords.objects.filter(uid=uid).update(**data_dict)
-	else:
-		data_dict["uid_id"] = uid
-		models.SensitiveWords.objects.create(**data_dict)
-	return JsonResponse({"status": True, "mes": "Save Success"})
-
-
-@csrf_exempt
-def check_article_checking(request):
-	"""检测文章接口"""
-	article = json.loads(request.POST.get("article"))
-	uid = request.session.get("info").get("uid")
-	words = models.SensitiveWords.objects.filter(uid=uid).first().words
-	words_list = words.split(";")
-	words_list.pop()
-	check_after = []
-	words_count = 0
-	for word in words_list:
-		if word in article:
-			text_template = f"<span class='alert alert-danger' style='padding:0 5px'>{word}</span>"
-			check_after.append(text_template)
-			article_list = article.split(word)
-			article = text_template.join(article_list)
-			words_count += 1
-	return JsonResponse({"status": True, "article": article, "wordsCount": words_count})
+    """保存敏感词接口"""
+    # 获取当前登陆的用户uid
+    uid = request.session.get("info").get("uid")
+    tag_list_js = request.POST.get("list")
+    tag_list = json.loads(tag_list_js)
+    words = ""
+    for word in tag_list:
+        words += f"{word['tag']};"
+    update_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    group = 1  # 埋点分组
+    data_dict = {"group": group, "update_time": update_time, "words": words}
+    exists = models.SensitiveWords.objects.filter(uid=uid).exists()
+    if exists:
+        models.SensitiveWords.objects.filter(uid=uid).update(**data_dict)
+    else:
+        data_dict["uid_id"] = uid
+        models.SensitiveWords.objects.create(**data_dict)
+    return JsonResponse({"status": True, "mes": "Save Success"})
