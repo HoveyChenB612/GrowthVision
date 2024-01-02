1db1694c387e24bbfc806008053b1cb19a75d862
HoveyChenB612 <chenhao852@icloud.com>
2024-01-02 15:57:55 +0800

    新增视频发布功能

--- mainsite/views/task.py
+++ mainsite/views/task.py
@@ -117,7 +117,7 @@ def history_data_update():
 	return JsonResponse({"status": True})
 
 
-@register_job(scheduler, "interval", minutes=1, id="new_data_update")
+@register_job(scheduler, "interval", minutes=30, id="new_data_update")
 def new_data_update():
 	"""10分钟更新数据"""
 
