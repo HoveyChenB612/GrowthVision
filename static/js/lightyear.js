956d7e877a417c95284963515bc9b4f09805e5d2
HoveyChenB612 <chenhao852@icloud.com>
2024-02-28 11:18:55 +0800

    修改数据展示页面功能，新增

--- static/js/lightyear.js
+++ static/js/lightyear.js
@@ -1,25 +1,25 @@
-var lightyear = function(){
-	
-	/**
-	 * 页面loading
-	 */
-	var pageLoader = function($mode) {
-		var $loadingEl = jQuery('#lyear-loading');
-		    $mode      = $mode || 'show';
-		if ($mode === 'show') {
-			if ($loadingEl.length) {
-				$loadingEl.fadeIn(250);
-			} else {
-				jQuery('body').prepend('<div id="lyear-loading"><div class="spinner-border text-primary" role="status"><span class="sr-only">Loading...</span></div></div>');
-			}
-		} else if ($mode === 'hide') {
-			if ($loadingEl.length) {
-				$loadingEl.fadeOut(250);
-			}
-		}
-		return false;
-	};
-	
+var lightyear = function () {
+
+    /**
+     * 页面loading
+     */
+    var pageLoader = function ($mode) {
+        var $loadingEl = jQuery('#lyear-loading');
+        $mode = $mode || 'show';
+        if ($mode === 'show') {
+            if ($loadingEl.length) {
+                $loadingEl.fadeIn(250);
+            } else {
+                jQuery('body').prepend('<div id="lyear-loading"><div class="spinner-border text-primary" role="status"><span class="sr-only">Loading...</span></div></div>');
+            }
+        } else if ($mode === 'hide') {
+            if ($loadingEl.length) {
+                $loadingEl.fadeOut(250);
+            }
+        }
+        return false;
+    };
+
     /**
      * 页面小提示
      * @param $msg 提示信息
@@ -32,57 +32,57 @@ var lightyear = function(){
      * @author CaiWeiMing <314013107@qq.com>
      */
     var tips = function ($msg, $type, $delay, $icon, $from, $align, $url) {
-        $type  = $type || 'info';
+        $type = $type || 'info';
         $delay = $delay || 1000;
-        $from  = $from || 'top';
+        $from = $from || 'top';
         $align = $align || 'center';
         $enter = $type == 'danger' ? 'animated shake' : 'animated fadeInUp';
-		$url = $url || url;
+        $url = $url || url;
         jQuery.notify({
-            icon: $icon,
-            message: $msg
-        },
-        {
-            element: 'body',
-            type: $type,
-            allow_dismiss: true,
-            newest_on_top: true,
-            showProgressbar: false,
-            placement: {
-                from: $from,
-                align: $align
+                icon: $icon,
+                message: $msg
             },
-            offset: 20,
-            spacing: 10,
-            z_index: 10800,
-            delay: $delay,
-            //timer: 1000,
-            animate: {
-                enter: $enter,
-                exit: 'animated fadeOutDown'
-            }
-        });
-		if($url!=''){
-			setTimeout(function(){
-				window.location.href=$url;
-			},$delay);
-		}
-		
+            {
+                element: 'body',
+                type: $type,
+                allow_dismiss: true,
+                newest_on_top: true,
+                showProgressbar: false,
+                placement: {
+                    from: $from,
+                    align: $align
+                },
+                offset: 20,
+                spacing: 10,
+                z_index: 10800,
+                delay: $delay,
+                //timer: 1000,
+                animate: {
+                    enter: $enter,
+                    exit: 'animated fadeOutDown'
+                }
+            });
+        if ($url != '') {
+            setTimeout(function () {
+                window.location.href = $url;
+            }, $delay);
+        }
+
     };
-	
-	var url = '';
-	
-	return {
+
+    var url = '';
+
+    return {
         // 页面小提示
-        notify  : function ($msg, $type, $delay, $icon, $from, $align, $url) {
+        notify: function ($msg, $type, $delay, $icon, $from, $align, $url) {
             tips($msg, $type, $delay, $icon, $from, $align, $url);
         },
-		url : function ($url){
-			url=$url;
-		},
+        url: function ($url) {
+            url = $url;
+        },
         // 页面加载动画
-		loading : function ($mode) {
-		    pageLoader($mode);
-		}
+        loading: function ($mode) {
+            pageLoader($mode);
+        }
     };
 }();
